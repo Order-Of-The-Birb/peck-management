213c84c883fc515441bb35a086489be75c99c8fd
[Linux] Maho-Yoshino <62778359+Maho-Yoshino@users.noreply.github.com>
2026-03-11 23:01:45 +0100

    updated database migrations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -33,6 +33,7 @@ class UserFactory extends Factory
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'two_factor_confirmed_at' => null,
+            'level' => 0
         ];
     }
 
@@ -43,18 +44,6 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
-        ]);
-    }
-
-    /**
-     * Indicate that the model has two-factor authentication configured.
-     */
-    public function withTwoFactor(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'two_factor_secret' => encrypt('secret'),
-            'two_factor_recovery_codes' => encrypt(json_encode(['recovery-code-1'])),
-            'two_factor_confirmed_at' => now(),
         ]);
     }
 }
---
+++ database/migrations/0001_01_01_000002_create_peck_tables.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create("peck_users", function (Blueprint $table) {
+            $table->unsignedBigInteger("gaijin_id")->primary();
+            $table->string("username")->unique();
+            $table->unsignedBigInteger("discord_id")->nullable();
+            $table->integer("tz")->nullable();
+            $table->enum("status", ["applicant","unverified","member","ex_member","alt"]);
+            $table->dateTime("joindate", precision: 0)->nullable();
+            $table->unsignedBigInteger("initiator")->nullable();
+
+            $table->foreign("initiator")->references("gaijin_id")->on("peck_users")->nullOnDelete();
+        });
+        Schema::create("peck_alts", function (Blueprint $table) {
+            $table->unsignedBigInteger("alt_id")->primary();
+            $table->unsignedBigInteger("owner_id");
+
+            $table->foreign("alt_id")->references("gaijin_id")->on("peck_users")->cascadeOnDelete();
+            $table->foreign("owner_id")->references("gaijin_id")->on("peck_users")->cascadeOnDelete();
+        });
+        Schema::create("peck_leave_info", function (Blueprint $table) {
+            $table->unsignedBigInteger("user_id")->primary();
+            $table->string("type")->notnullable();
+
+            $table->foreign("user_id")->references("gaijin_id")->on("peck_users")->cascadeOnDelete();
+        });
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists("peck_users");
+        Schema::dropIfExists("peck_alts");
+        Schema::dropIfExists("peck_leave_info");
+    }
+};
